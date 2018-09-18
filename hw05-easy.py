63c6d2889a89d61bbeda50357c59793ab30dcc74
yura <ogay_yura@mail.ru>
2018-09-18 12:27:15 +0300

    Выполнено задание easy lesson-5

--- hw05-easy.py
+++ hw05-easy.py
@@ -3,8 +3,28 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+for i in range(1, 10):
+    dirname = 'dir_' + str(i)
+    os.mkdir(dirname)
+
+for i in range(1, 10):
+    dirname = 'dir_' + str(i)
+    os.rmdir(dirname)
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+for name in os.listdir():
+    if os.path.isdir(name):
+        print(name)
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+from shutil import copyfile
+
+a3_name = (__file__)
+a3_copy = a3_name + '_bkp'
+copyfile(a3_name, a3_copy)
