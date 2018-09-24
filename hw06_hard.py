0d18766b893942129230b4f1df158fde000ea6b6
yura <ogay_yura@mail.ru>
2018-09-24 13:37:35 +0300

    Выполнено задание hard lesson-6

--- hw06_hard.py
+++ hw06_hard.py
@@ -11,3 +11,48 @@
 # Задача - 2
 # Доработайте нашу фабрику, создайте по одному классу на каждый тип, теперь надо в классе фабрика
 # исходя из типа игрушки отдавать конкретный объект класса, который наследуется от базового - Игрушка
+
+class Toy:
+    def __init__(self, name, color, type):
+        self._name = name
+        self._color = color
+        self._type = type
+
+
+class Create_Toy(Toy):
+
+    def purchase(self):
+        print('Ведется закупка сырья для', self._name)
+
+    def building(self):
+        print('Ведется сборка изделия -', self._name)
+
+    def coloring(self):
+        print('Окраска изделия -', self._name)
+
+
+class Hard_toy(Create_Toy):
+
+    def comp(self):
+        print('Ведется обработка пластика, металла', self._name)
+
+
+class Soft_toy(Create_Toy):
+
+    def comp(self):
+        print('Ведется обработка мягкого изделия', self._name)
+
+
+toy_1 = Soft_toy('bear', 'white', 'soft toy')
+toy_1.purchase()
+toy_1.comp()
+toy_1.building()
+toy_1.coloring()
+print(type(toy_1))
+
+toy_2 = Hard_toy('train', 'black', 'hard toy')
+toy_2.purchase()
+toy_2.comp()
+toy_2.building()
+toy_2.coloring()
+print(type(toy_2))
